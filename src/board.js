2dfcfc0018c272c662100d479ae2ff68f6aeb34b
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2022-11-04 19:50:33 -0700

    Create home page with multiple notes

--- src/board.js
+++ src/board.js
@@ -5,12 +5,19 @@ import './board.css'
 class Board extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            notes: Array(3).fill(null)
+        }
     }
 
     render() {
         return(
-            <div className="Background">
-                < Note />
+            <div className="Board">
+                {this.state.notes.map((note, index) => {
+                    return(
+                        < Note />
+                    );
+                })}
             </div>
         );
     }
