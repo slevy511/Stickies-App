e573bc580096a552095f436ea723f573df526d5b
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2022-11-18 22:15:10 -0800

    Link database to displayed information (#13)
    * Implement frontend retrieval and insertion of data
    
    server.js: Include changes made in backend-development branch
    index.js: Initialize the user's state when a user logs in via the "login" function
    Login.js, Register.js: Switch to use new login function defined in index.js
    
    Board.js: Implement display of notes with content determined by backend. Implement insertion and deletion of notes.
    Note.js: Allow notes to display contents based on information stored in the database. Allow for updating and deletion of notes.
    
    * Minor programming style changes
    
    * Remove polls and charts
    
    Updated project spec: No longer implementing polls and charts

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Axios from 'axios';
 import LoginForm from './components/Login'
 import RegistrationForm from './components/Register'
 import Board from './components/Board'
@@ -7,28 +8,45 @@ import logo from './logo.png'
 
 class App extends React.Component{
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      active: 'LoginForm'
+      active: 'LoginForm',
+      user: null,
+      boardNum: 0,
+      boards: []
     }
 
-    this.setActive = this.setActive.bind(this);
+    this.setActive = this.setActive.bind(this)
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
+  }
+
+  async login(uname){
+    const all_boards = await Axios.get("http://localhost:8000/api/get-all-boards/" + uname)
+    const bds = all_boards.data
+    this.setState({active: 'Board', user: uname, boardNum: 0, boards: bds})
   }
 
   setActive(s){
     this.setState({active: s})
   }
 
+  logout(){
+    this.setState({active: 'LoginForm', user: null, boardNum: 0, boards: []})
+  }
+
   render(){
     return (
       <div>
-
-       
-        
-
-        {this.state.active === 'LoginForm' ? <LoginForm setActive={this.setActive}/> : null}
-        {this.state.active === 'RegistrationForm' ? <RegistrationForm setActive={this.setActive} /> : null}
-        {this.state.active === 'Board' ? <Board setActive={this.setActive} /> : null}
+        {this.state.active === 'LoginForm' ?
+        <LoginForm login={this.login} setActive={this.setActive} />
+        : null}
+        {this.state.active === 'RegistrationForm' ?
+        <RegistrationForm login={this.login} setActive={this.setActive} />
+        : null}
+        {this.state.active === 'Board' ?
+        <Board logout={this.logout} user={this.state.user} activeBoard={this.state.boards[this.state.boardNum]}/>
+        : null}
       </div>
     );
   }
